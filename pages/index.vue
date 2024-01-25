a38369639e06281207575575156a4faeb79fadd9
Shivam Mishra <113275391+Shivammishra03@users.noreply.github.com>
2024-01-25 23:57:59 +0530

    changes added

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,3 @@
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium eius delectus laudantium reprehenderit voluptatibus obcaecati maxime enim, aut itaque expedita?</p>
     </div>
 </template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-
-</style>
